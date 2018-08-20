919f7a6ceae273da2fef5f586b41da6915d32fd9
Roderick Menor <rodpajmen@gmail.com>
2018-08-20 11:28:53 -1000

    Finished #8

--- exercises.py
+++ exercises.py
@@ -56,7 +56,7 @@ for i in range(len(cars)):
 i = 0
 while i < len(cars):
     print(cars[i])
-    i + 1
+    i += 1
     
 #6  No More Tears
 # Create a for loop that will iterate through the cyber attacks list and prints the following: 
@@ -68,11 +68,17 @@ while i < len(cars):
 #DO NOT use numbers in your string.
 
 cyber_attacks = ['Wannacry', 'Petya', 'Locky', 'Krack Attack', 'Sambacry']
-
+for attack in cyber_attacks:
+     print('The attack at ' + str(cyber_attacks.index(attack)) + ' is ' + attack + '.')
 
 #7 Even
 # Declare a variable named even_list and assign it an empty list. Next, write a for loop that will place 25 even numbers starting from 0 into the even_list list. Print the even_list variable to see your results. 
 
+even_list = []
+for number in range(0, 51):
+    if number%2 == 0:
+        even_list.append(number)
+print(even_list)
 
 #8 Sum Up
 # Create a function named add_up which takes a parameter num. In the code block inside the function, create a variable named sum and assign it a number value of 0. Next, create a for loop that will iterate through a list of numbers using the range function that will be determined by the num parameter and will sum up all the numbers in the list and store it to the sum variable. Print the sum variable to see your results.
